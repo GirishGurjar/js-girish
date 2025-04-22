508300210516de8e4dbd32c0a082e339425f9bb4
GIRISH97137 <131401634+GIRISH97137@users.noreply.github.com>
2025-04-22 12:10:16 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId =1223233;
+let accountEmail = "girish@gmail.com";
+var accountPassword = "12345"
+ accountCity = "Gwalior"
+//accountId = 2  // not allowed
+let accountState;
+
+accountEmail = "abc@abc.com"
+accountPassword = 1233434
+accountCity = "Delhi"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
