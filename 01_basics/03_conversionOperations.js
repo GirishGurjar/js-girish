259b5a95039dbc313acc4e927a0e784979ec0afc
GIRISH97137 <131401634+GIRISH97137@users.noreply.github.com>
2025-04-22 12:44:54 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -15,7 +15,7 @@ let valueInNumber = Number(score);
 let IsLoggedIn ="1avc"
 
 let booleanIsLoggedIn = Boolean(IsLoggedIn);
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 
 // true = 1; false = 0
@@ -25,5 +25,31 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+/***************************************OPERATIONS******************************/
+
+let value =  3
+let NegValue = -value;
+
+// console.log(NegValue);
+// console.log(2**2);
+
+
+let str1 = "hello";
+let str2 = "ji"
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1"+ 2);
+// console.log(1+ "2");
+// console.log("1"+ 2 + 2);
+// console.log(1+ 2 + "2");
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
+
