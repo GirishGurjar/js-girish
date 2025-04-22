b8924957e6c86ff8dd7bebfc751273a6df47e8e6
GIRISH97137 <131401634+GIRISH97137@users.noreply.github.com>
2025-04-22 12:28:26 +0000

    conversionofDatatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,29 @@
+let score = "girish";
+
+//console.log(typeof score);
+//console.log (typeof (score));
+
+let valueInNumber = Number(score);
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//33 = 33
+//"33abc" = NaN
+// true = 1 , false = 0
+
+let IsLoggedIn ="1avc"
+
+let booleanIsLoggedIn = Boolean(IsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+// true = 1; false = 0
+// "" = false
+// "girish"= true
+
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
