c37faeec1e671ba96fbdd85f541ba2883038a63b
GIRISH97137 <131401634+GIRISH97137@users.noreply.github.com>
2025-04-24 21:03:32 +0000

    datesin07

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,33 @@
+const name = "girish";
+const repoCount = 50;
+console.log(name + repoCount);
+console.log(`hello my name is ${name} and my repoCount is ${repoCount}` );
+
+const gameName = new String(`girish-gurjar`)
+
+// console.log(gameName[8]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toLocaleUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf(`i`));
+
+
+// const NewString =  gameName.substring(0,4)
+// console.log(NewString);
+
+const anotherString =  gameName.slice(0,4)
+console.log(anotherString);
+
+
+
+const NewStringOne = "    giris    "
+console.log(NewStringOne);
+console.log(NewStringOne.trim());
+
+
+const url = "https://javawithgirish/girish%20gurjar"
+
+console.log(url.replace('%20','-'));
+
+console.log(gameName.split('-'));
---
+++ 01_basics/07_datesin.js
@@ -0,0 +1,13 @@
+let myDate = new Date()
+
+console.log(myDate);
+
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(typeof myDate);
+
+let myCreateDate = new Date(2023,0,24,5,3)
+
+console.log(myCreateDate.toDateString())
+console.log(myCreateDate.toLocaleDateString())
