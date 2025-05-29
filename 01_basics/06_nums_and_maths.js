e70f7ae644b2695cea74729c7d33d8180e149501
GIRISH97137 <131401634+GIRISH97137@users.noreply.github.com>
2025-05-29 11:24:27 +0000

    commit

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -1,23 +1,25 @@
-const score = 400
+const score = 400;
+//console.log(score);
 
 const balance = new Number(100)
 // console.log(balance);
 
+//console.log(balance.toString().length);
 
 // console.log(balance.toFixed(2))
 
-const hundreds = 100000
+const hundreds = 10000000
 // console.log(hundreds.toLocaleString('en-IN'))
 
 const otherNumber = 134.8954
 
-// console.log(otherNumber.toPrecision(4));
+ //console.log(otherNumber.toPrecision(4));
 
 // console.log(hundreds.toString().length)
 
 
 //******************************Math**************************************
-console.log(Math)
+//console.log(Math)
 
 // console.log(Math.abs(-4))
 // console.log(Math.round(4.333))
@@ -26,7 +28,7 @@ console.log(Math)
 // console.log(Math.min(4,5,2,8))
 // console.log(Math.max(4,5,2,8))
 
-// console.log(Math.random());
+// console.log(Math.random());   // gives values btw 0 and 1
 // console.log(Math.random()*10);
 // console.log((Math.random()*10)+1);
 // console.log((Math.floor(Math.random()*10)+1));
@@ -35,3 +37,8 @@ console.log(Math)
 const max = 20
 const min = 10
 console.log(Math.floor(Math.random()* (max - min + 1 ))+ min);  //formula
+
+
+const upper = 15
+const lower = 5
+console.log(Math.floor(Math.random()* (upper - lower + 1) ) + min );
