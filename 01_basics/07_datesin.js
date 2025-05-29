9e1599ea9f1cacbfdc48dd0dc26bee1195da48f2
GIRISH97137 <131401634+GIRISH97137@users.noreply.github.com>
2025-05-29 11:40:40 +0000

    commit

--- 01_basics/07_datesin.js
+++ 01_basics/07_datesin.js
@@ -2,13 +2,35 @@ let myDate = new Date()
 
 console.log(myDate);
 
-console.log(myDate.toString());
-console.log(myDate.toDateString());
-console.log(myDate.toLocaleDateString());
-console.log(typeof myDate);
 
-let myCreateDate = new Date(2023,0,24,5,3)
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toUTCString());
 
-console.log(myCreateDate.toDateString())
-console.log(myCreateDate.toLocaleDateString())
 
+// console.log(typeof myDate);
+
+let myCreateDate = new Date("01-14-2025")
+
+//console.log(myCreateDate.toDateString())
+//console.log(myCreateDate.toLocaleDateString())
+
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+
+// 
+let newDate = new Date()
+
+console.log(newDate);
+console.log(newDate.getMonth());
+
+newDate.toLocaleString('default',{
+
+  weekday : "long",
+   month : "short" 
+})
+console.log(newDate);
