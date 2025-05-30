c5533d7ff08d0106d4c12dc1502f4861f16e6964
GIRISH97137 <131401634+GIRISH97137@users.noreply.github.com>
2025-05-30 18:28:43 +0000

    commit

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,3 +1,43 @@
 // Singleton
 
-//v
+// object literals
+
+//object.create // constructor method , use singleton
+
+const mySym = Symbol("key1")
+ 
+const JsUser = {
+    name : "Girish",
+    "full name" : "Girish Gurjar",
+    [mySym] : "key1",
+    age : 18,
+    location : "Delhi",
+    email: "g@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday","Sunday"]
+}
+// console.log(JsUser.email);
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+
+//JsUser.email = "Girish@changeobject.com"   //to change the anything in the object
+//Object.freeze(JsUser) // to freeze the object for no further changes accepted
+//JsUser.email = "girish@gurjar.com"
+//console.log(JsUser);
+
+
+JsUser.greeting = function(){
+ console.log("Hello JS user");
+    
+}
+
+JsUser.greetingTwo = function(){
+ console.log(`Hello JS user,${this.name}`);
+    
+}
+console.log(JsUser.greeting);
+console.log(JsUser.greetingTwo);
+
+
+
