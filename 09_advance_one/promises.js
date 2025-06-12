8924e1112e32fc80eacaec9e8304bef4dc21ad41
GIRISH97137 <131401634+GIRISH97137@users.noreply.github.com>
2025-06-12 09:33:49 +0000

    c

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -129,9 +129,9 @@ consumePromiseFive().then((username) => {
 
 fetch('https://jsonplaceholder.typicode.com/users')
     .then((response) => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
+        // if (!response.ok) {
+        //     throw new Error('Network response was not ok');
+        // }
         return response.json();
     })
     .then((data) => {
