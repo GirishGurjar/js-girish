b77b604a0de14547d23dcad5772458b3b01b0e74
GIRISH97137 <131401634+GIRISH97137@users.noreply.github.com>
2025-06-12 08:56:38 +0000

    promises

---
+++ 09_advance_one/promises.js
@@ -0,0 +1,41 @@
+//fetch('https://api.github.com/users/GirishGurjar').then().catch();
+
+const promiseOne = new Promise(function(resolve, reject){
+    //Do an async task
+    //DB call, API call,network call, crypto call etc.
+
+setTimeout(() => {
+   console.log('Async task completed');
+   resolve('Task completed');
+    
+}, 1000);
+
+});
+
+promiseOne.then(function(value){
+    console.log(value);
+})
+
+
+new Promise(function(resolve, reject){
+
+setTimeout(function(){
+    console.log('Async task 2 completed');
+    resolve('Task 2 completed');
+}, 1000);
+
+}).then(function(value){
+    console.log(value);
+}
+)
+
+
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(user){
+       // console.log('Async task 3 completed');
+        resolve({username: 'Girish', age: 25}); 
+    }, 1000);
+})
+promiseThree.then(function(user){
+    console.log(user)
+})
