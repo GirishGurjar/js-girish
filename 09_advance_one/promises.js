ba2cea5941ac42db26786c1cb0eb5c0283c0b2c7
GIRISH97137 <131401634+GIRISH97137@users.noreply.github.com>
2025-06-12 09:31:44 +0000

    promises

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -64,4 +64,79 @@ const username = promiseFour.then((user) => {
     console.log(username);
 }).catch(function(error){
     console.error(error);
+}).finally(function(){
+    console.log('Promise completed');
 })
+
+const promiseFive = new Promise(function(resolve, reject){
+    setTimeout (function(){
+let error = true;
+if(error) {
+    resolve({
+        username: 'js',
+        password: 112233,
+        email: 'g@gmail.com'
+    }) 
+    }else{
+    reject('Error: JS went wrong');
+}
+
+    }, 1000);
+})
+
+async function consumePromiseFive() {
+    try {
+        const response = await promiseFive;
+        console.log(response);
+        return response.username;
+
+    } catch (error) {
+        console.error(error);
+
+    } finally {
+        console.log('Promise Five completed');
+    }
+}
+consumePromiseFive().then((username) => {
+    console.log(username);
+}
+).catch((error) => {
+    console.error('Error in consumePromiseFive:', error);
+}
+).finally(() => {
+    console.log('Finally block executed for consumePromiseFive');
+}
+);
+
+
+// async function getAllUsers() {
+
+//  try {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users');
+
+//     const data =await response.json();
+//     console.log(data);
+    
+//     }
+// catch(error){
+//     console.log("E :", error);
+
+    
+// }
+// }
+// getAllUsers()
+
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((error) => {
+        console.error('Error fetching users:', error);
+    });
